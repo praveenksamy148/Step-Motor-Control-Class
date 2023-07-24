7cf99b8ef078f5c1888cd8edcda6e5b5ca31f4d7
Praveen Kandasamy <63980672+praveenksamy148@users.noreply.github.com>
2023-07-24 15:30:31 -0700

    Update motorClass.ino

--- motorClass.ino
+++ motorClass.ino
@@ -2,10 +2,13 @@
 #include <Stepper.h> 
 #include <SoftwareSerial.h> 
 
-const byte rxPin = 6; 
-const byte txPin = 7; 
-// SoftwareSerial slave1(rxPin, txPin); 
-String sMotor;
+
+const byte rxPin1 = 6; 
+const byte txPin1 = 7; 
+
+//Arduino COM 5 
+SoftwareSerial mySerial(rxPin1, txPin1); 
+
 String sCommand;
 bool bStep = false;
 bool bDelayStop;
@@ -17,38 +20,44 @@ Motorbase* motor1 = NULL;
 const int stepsPerRevolution = 2038; 
 void setup() {
   Serial.begin(9600); 
-  Serial.println("Started slave 1"); 
+  mySerial.begin(9600); 
+  Serial.println("Started motor 1"); 
   motor1 = new Smallmotor();
   motor1->initialize(2038, 8, 9, 10, 11); 
-   Serial.println("Motor initialized successfully"); 
-  // slave1.begin(9600); 
+  Serial.println("Motor initialized successfully");  
   motor1->speedSet(8); 
   motor1->step();
+  Serial.print("reading character: "); 
+  while(true){
+    if(mySerial.available()){
+      Serial.println(mySerial.read());
+      break; 
+    }    
+  }
 }
 
 bool GetCommand(String& sCommand)
 {
- 
-  if(!Serial.available()){
+  if(!mySerial.available()){
+    // Serial.println("not available"); 
     return false; 
   }
-  char sig= Serial.read();
-  if(sig != '$')
-   return false;
-  int iCount =0;
-  
+  int iCommand =0; 
   sCommand ="";
-  while (iCount < 3)
+  // Serial.println("going to read"); 
+  while (iCommand < 3)
   {
-    if (Serial.available())
+    // Serial.println("reading string"); 
+    if (mySerial.available())
     {
-      char rc= Serial.read();
-      Serial.print (" - "); 
-      Serial.println(rc);
+      char rc= mySerial.read();
+      // Serial.print (" - "); 
+      // Serial.println(rc);
       sCommand += rc;
-      iCount++;
+      iCommand++;
     } 
   }
+  Serial.println(sCommand); 
  return true;
 }
 
@@ -56,6 +65,7 @@ void ProcessCommand(String sCommand, bool& bStep)
 {
   bool bDelayStop = false;
   int delayMS = 0;
+  Serial.println(sCommand); 
   if(sCommand == "000")
  {
    bStep = false;
@@ -80,7 +90,6 @@ void ProcessCommand(String sCommand, bool& bStep)
 
  if(sCommand == "003")
  {
-
    motor1->speedSet(5);
    return;
  }
@@ -88,23 +97,23 @@ void ProcessCommand(String sCommand, bool& bStep)
 
  if(sCommand == "004")
  {
-
    motor1->speedSet(8);
    return;
  }
  if(sCommand == "005")
  {
-
    motor1->speedSet(10);
    return;
  }
 
  if(sCommand == "006")
  {
+   Serial.println("6 command working");
    bDelayStop = true;
-   delayMS = 1000;
-   bStep = false; 
-   return; 
+   motor1->delayMotor(1000); 
+   motor1->stop(); 
+   bStep = false;   
+   return;  
  }
  if(sCommand == "007")
  {
@@ -120,10 +129,8 @@ void ProcessCommand(String sCommand, bool& bStep)
    bDelayStop = true;
    delayMS = 5000;
    motor1->delayMotor(delayMS); 
-  //  motor1->stop(); 
    bStep = false; 
    return; 
-  //  bStep = false; 
  }
 
 
@@ -131,7 +138,7 @@ void ProcessCommand(String sCommand, bool& bStep)
 
 }
 void loop() {
-// Serial.println("starting"); 
+
  if(GetCommand(sCommand)){
   ProcessCommand(sCommand,bStep);
  }
